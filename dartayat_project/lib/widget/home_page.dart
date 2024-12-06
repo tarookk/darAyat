d691ddb12984b2c92cde9f1501afc31eba1acabf
Ahmed Tarek Gaber <ahmad200200.tarek.200200@gmail.com>
2024-12-06 23:15:08 +0200

    ok

--- dartayat_project/lib/widget/home_page.dart
+++ dartayat_project/lib/widget/home_page.dart
@@ -28,6 +28,8 @@ class _HomePageState extends State<HomePage> {
       BuildContext context, String title, Function(String) onSave) {
     TextEditingController controller = TextEditingController();
 
+    FocusScope.of(context).requestFocus(FocusNode());
+
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -35,6 +37,7 @@ class _HomePageState extends State<HomePage> {
         content: TextField(
           controller: controller,
           decoration: InputDecoration(hintText: "اكتب هنا..."),
+          autofocus: true,  
         ),
         actions: [
           TextButton(
@@ -56,139 +59,140 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: Color(0xffAE9453),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Image.asset(
-                'asset/image.png',
-                height: 100,
-                width: 100,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 200),
-            child: Text('تسجيل الدخول',
-                style: TextStyle(color: Colors.black, fontSize: 22)),
-          ),
-          SizedBox(height: 100),
-          ElevatedButton(
-            onPressed: () {
-              showInputDialog(
-                context,
-                "أدخل اسم المستخدم",
-                (text) {
-                  setState(() {
-                    userName = text;
-                  });
-                },
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20),
-              shadowColor: Colors.black.withOpacity(0.3),
-              elevation: 5,
-            ),
-            child: Text(userName.isEmpty
-                ? "أدخل اسم المستخدم"
-                : "اسم المستخدم: $userName"),
-          ),
-          SizedBox(height: 40),
-          ElevatedButton(
-            onPressed: () {
-              showInputDialog(
-                context,
-                "أدخل كود التعريف",
-                (text) {
-                  setState(() {
-                    userCode = text;
-                  });
-                },
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20),
-              shadowColor: Colors.black.withOpacity(0.3),
-              elevation: 5,
-            ),
-            child: Text(userCode.isEmpty
-                ? "أدخل كود التعريف"
-                : "كود التعريف: $userCode"),
-          ),
-          SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [SizedBox(width: 8),
-              TextButton(
-                onPressed: () {
-                  print("تم الضغط على التواصل مع الإدارة");
-                },
-                child: Text(
-                  "تواصل مع الإدارة",
-                  style: TextStyle(
-                    color: Color(0xffAE9453),
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: SingleChildScrollView(  
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Color(0xffAE9453),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Image.asset(
+                  'asset/image.png',
+                  height: 100,
+                  width: 100,
                 ),
               ),
-              
-              TextButton(
-                onPressed: () {
-                  print("تم الضغط على التواصل مع الإدارة");
-                },
-                child: Text(
-                  'هل تواجه مشكلة؟',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 60),
-            child: ElevatedButton(
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 200),
+              child: Text('تسجيل الدخول',
+                  style: TextStyle(color: Colors.black, fontSize: 22)),
+            ),
+            SizedBox(height: 100),
+            ElevatedButton(
               onPressed: () {
-                print("تم الضغط على تسجيل");
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => StudentManagement(),
-                    ));
+                showInputDialog(
+                  context,
+                  "أدخل اسم المستخدم",
+                  (text) {
+                    setState(() {
+                      userName = text;
+                    });
+                  },
+                );
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 210, 180, 104),
-                disabledBackgroundColor: Colors.black,
+                foregroundColor: Colors.black,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20),
                 shadowColor: Colors.black.withOpacity(0.3),
                 elevation: 5,
               ),
-              child: Text("تسجيل",
-                  style: TextStyle(
-                      color: const Color.fromARGB(255, 251, 248, 248),
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold)),
+              child: Text(userName.isEmpty
+                  ? "أدخل اسم المستخدم"
+                  : "اسم المستخدم: $userName"),
             ),
-          ),
-        ],
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                showInputDialog(
+                  context,
+                  "أدخل كود التعريف",
+                  (text) {
+                    setState(() {
+                      userCode = text;
+                    });
+                  },
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20),
+                shadowColor: Colors.black.withOpacity(0.3),
+                elevation: 5,
+              ),
+              child: Text(userCode.isEmpty
+                  ? "أدخل كود التعريف"
+                  : "كود التعريف: $userCode"),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 8),
+                TextButton(
+                  onPressed: () {
+                    print("تم الضغط على التواصل مع الإدارة");
+                  },
+                  child: Text(
+                    "تواصل مع الإدارة",
+                    style: TextStyle(
+                      color: Color(0xffAE9453),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print("تم الضغط على التواصل مع الإدارة");
+                  },
+                  child: Text(
+                    'هل تواجه مشكلة؟',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: ElevatedButton(
+                onPressed: () {
+                  print("تم الضغط على تسجيل");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StudentManagement(),
+                      ));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 210, 180, 104),
+                  disabledBackgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                  shadowColor: Colors.black.withOpacity(0.3),
+                  elevation: 5,
+                ),
+                child: Text("تسجيل",
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 251, 248, 248),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
