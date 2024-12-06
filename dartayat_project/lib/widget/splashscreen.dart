74ca7ef0f8c88618784f4880cb90034023f410cb
Ahmed Tarek Gaber <ahmad200200.tarek.200200@gmail.com>
2024-12-06 22:42:47 +0200

    ok

--- dartayat_project/lib/widget/splashscreen.dart
+++ dartayat_project/lib/widget/splashscreen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToHome() async {
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 2));
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => HomePage ()),
