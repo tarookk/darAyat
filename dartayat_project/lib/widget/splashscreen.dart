241509da59d92284b5b12d147652309eccef097d
Ahmed Tarek Gaber <ahmad200200.tarek.200200@gmail.com>
2024-12-08 13:40:15 +0200

    chane new

--- dartayat_project/lib/widget/splashscreen.dart
+++ dartayat_project/lib/widget/splashscreen.dart
@@ -1,10 +1,5 @@
-
-
 import 'package:dartayat_project/widget/home_page.dart';
 import 'package:flutter/material.dart';
-
-
-
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -19,35 +14,42 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToHome() async {
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 50));
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(builder: (context) => HomePage ()),
-      //
+      MaterialPageRoute(builder: (context) => HomePage()),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffAE9453), 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                
-                Center(
-                  child: Image.asset('asset/image 1.png',height:50,width: 50,color: Color.fromARGB(255, 73, 56, 13),
-                              ),
-                ),
-              ],
-            ),
-            Image.asset('asset/image2.png',height: 200,width: 200,), 
-            
-           
-          ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF4A2500), // اللون البني الداكن
+              Color(0xFFAE9453), // اللون الذهبي الفاتح
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              SizedBox(height: 20),
+              Image.asset(
+                'asset/image.png',
+                height: 150,
+                width: 150
+              ),
+            ],
+          ),
         ),
       ),
     );
